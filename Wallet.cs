6278ca7980c66083594ac5d813a50ae0bce21651
Ramunas Zavistanavicius <ramunas.zavistanavicius@devbridge.com>
2018-03-23 13:44:37 +0200

    Added Wallet

---
+++ Wallet.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+namespace Blockchain
+{
+    public class Wallet
+    {
+        public string privateKey;
+        public string publicKey;
+
+        public Wallet()
+        {
+            GenerateKeyPair();
+        }
+
+        public void GenerateKeyPair()
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            RSAParameters rsaKeyInfo = rsa.ExportParameters(true);
+        }
+    }
+}
